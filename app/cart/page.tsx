52ea01747486400bb20dce4d9a4f9755b8a0b92a
Granit Halimi <halimigranit6@gmail.com>
2025-07-23 20:41:26 +0200

    type error fixed

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import Header from "@/components/header";
 import Image from "next/image";
 import { fira, montserrat, poppins } from "../font/fonts";
@@ -10,53 +12,83 @@ export default async function Page() {
   const { data } = await supabase.auth.getUser();
   const cart = await supabase
     .from("carts")
-    .select("cp:carts_products(product_id, qty, p:products(*))")
+    .select(
+      "carts_products(qty, product:products(thumbnail, title, price, availabilityStatus))"
+    )
     .eq("user_id", data?.user?.id)
     .single();
-  console.log(cart)
 
+  const products = cart.data?.carts_products
   return (
     <div className="min-h-screen">
       <Header />
-      {cart.data?.cp && cart.data.cp.length > 0 ? (
+      {products && products.length > 0 ? (
         <div className="w-10/12 bg-white shadow-lg shadow-black/10 rounded-lg mx-auto p-10">
-          <h1 className={`${fira.className} text-3xl drop-shadow-lg uppercase text-center mb-5`}>Cart Products</h1>
+          <h1
+            className={`${fira.className} text-3xl drop-shadow-lg uppercase text-center mb-5`}
+          >
+            Cart Products
+          </h1>
           <div className="w-full text-end">Price</div>
           <hr />
-          {cart.data?.cp.map((c, ind) => (
+          {products.map((c:any, ind: number) => (
             <div
               key={ind}
               className="w-full rounded-lg my-3 flex justify-between"
             >
               <div className="flex">
-                <Image width={150} height={150} src={c.p.thumbnail} alt={c.p.title} />
+                <Image
+                  width={150}
+                  height={150}
+                  src={c.product.thumbnail}
+                  alt={c.product.title}
+                />
                 <div className="flex flex-col items-start">
-                  <h1 className={`${poppins.className}`}>{c.p.title}</h1>
-                  <h1 className={`${fira.className} text-green-400`}>{c.p.availabilityStatus}</h1>
+                  <h1 className={`${poppins.className}`}>{c.product.title}</h1>
+                  <h1 className={`${fira.className} text-green-400`}>
+                    {c.product.availabilityStatus}
+                  </h1>
                   <div className="flex justify-start px-2 gap-3 border  rounded-lg">
-                    <button className="text-red-500">-</button>{c.qty}<button className="text-green-500">+</button>
+                    <button className="text-red-500">-</button>
+                    {c.qty}
+                    <button className="text-green-500">+</button>
                   </div>
                 </div>
               </div>
-              <div className={`${poppins.className} text-green-400 `}>${c.p.price}</div>
+              <div className={`${poppins.className} text-green-400 `}>
+                ${c.product.price}
+              </div>
             </div>
           ))}
           <hr />
-          <h1 className={`${montserrat.className} text-center mt-4`}>Place Order</h1>
+          <h1 className={`${montserrat.className} text-center mt-4`}>
+            Place Order
+          </h1>
           <form className="flex flex-col w-1/4 gap-3 mx-auto">
             <div className="flex flex-col">
               <label>First Name:</label>
-              <input className="py-1 ps-2 rounded-lg border border-black" placeholder="John" />
+              <input
+                className="py-1 ps-2 rounded-lg border border-black"
+                placeholder="John"
+              />
             </div>
             <div className="flex flex-col">
               <label>Last Name:</label>
-              <input className="py-1 ps-2 rounded-lg border border-black" placeholder="Smith" />
+              <input
+                className="py-1 ps-2 rounded-lg border border-black"
+                placeholder="Smith"
+              />
             </div>
             <div className="flex flex-col">
               <label>Adress</label>
-              <input className="py-1 ps-2 rounded-lg border border-black" placeholder="Boulevar street nr.99" />
+              <input
+                className="py-1 ps-2 rounded-lg border border-black"
+                placeholder="Boulevar street nr.99"
+              />
             </div>
-            <button className={`${poppins.className} px-10 py-2 bg-yellow-400 shadow-lg rounded-full flex justify-center  gap-2 hover:bg-yellow-500 duration-300`}>
+            <button
+              className={`${poppins.className} px-10 py-2 bg-yellow-400 shadow-lg rounded-full flex justify-center  gap-2 hover:bg-yellow-500 duration-300`}
+            >
               <TruckElectric /> Place Order
             </button>
           </form>
