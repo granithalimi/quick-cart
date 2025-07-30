2bfc138620812bccd7f307e813b700860cd6a76a
Granit Halimi <halimigranit6@gmail.com>
2025-07-30 02:20:30 +0200

    added loading for orders

---
+++ app/orders/loading.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/header";
+
+export default function Loading() {
+  return (
+    <>
+      <Header />
+      <div className="w-full flex justify-center items-center mt-10">
+        <div className="w-10 h-10 border-4 border-gray-200 border-t-blue-400 rounded-full animate-spin mx-auto" />
+      </div>
+    </>
+  )
+}
